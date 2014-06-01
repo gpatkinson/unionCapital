bb64fc713a9904b1cce123c8140fc1caeaac000f
Liam Morley <liam@carpeliam.com>
2014-06-01 15:31:35 -0400

    attempt to add timestamps to transactions/events
    FIXME This doesn't seem to work as is; needs some debugging.

--- collections/events.js
+++ collections/events.js
@@ -33,8 +33,33 @@ Events = new Meteor.Collection('events', {
             url: {
                        type: String,
                        label: 'URL to the event'
-                     }
-          }
+                     },
+            createdAt: {
+                type: Date,
+                autoValue: function() {
+                    if (this.isInsert) {
+                        return new Date;
+                    } else if (this.isUpsert) {
+                        return {$setOnInsert: new Date};
+                    } else {
+                        this.unset();
+                    }
+                },
+                denyUpdate: true
+            },
+            // Force value to be current date (on server) upon update
+            // and don't allow it to be set upon insert.
+            updatedAt: {
+                type: Date,
+                autoValue: function() {
+                    if (this.isUpdate) {
+                        return new Date();
+                    }
+                },
+                denyInsert: true,
+                optional: true
+            }
+  }
 });
 
 Events.allow({
--- collections/transaction.js
+++ collections/transaction.js
@@ -9,7 +9,32 @@ Transactions = new Meteor.Collection('transactions', {
                type: String,
                label: 'ID of related Event'
                }
-          }
+          },
+      createdAt: {
+          type: Date,
+          autoValue: function() {
+              if (this.isInsert) {
+                  return new Date;
+              } else if (this.isUpsert) {
+                  return {$setOnInsert: new Date};
+              } else {
+                  this.unset();
+              }
+          },
+          denyUpdate: true
+      },
+      // Force value to be current date (on server) upon update
+      // and don't allow it to be set upon insert.
+      updatedAt: {
+          type: Date,
+          autoValue: function() {
+              if (this.isUpdate) {
+                  return new Date();
+              }
+          },
+          denyInsert: true,
+          optional: true
+      }
 });
 
 Transactions.allow({
